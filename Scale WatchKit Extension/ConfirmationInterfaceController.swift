b4dc88b39b65770a88bdf32f8c3f5f94f6058b5a
Justyn Henman <justynlive@gmail.com>
2020-09-28 15:24:20 +0100

    add slider, kilogram label and button

---
+++ Scale WatchKit Extension/ConfirmationInterfaceController.swift
@@ -0,0 +1,16 @@
+//
+//  ConfirmationInterfaceController.swift
+//  Scale WatchKit Extension
+//
+//  Created by Justyn Henman on 28/09/2020.
+//  Copyright © 2020 Justyn Henman. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class ConfirmationInterfaceController: WKInterfaceController {
+
+    
+    
+}
--- Scale WatchKit Extension/InterfaceController.swift
+++ Scale WatchKit Extension/InterfaceController.swift
@@ -10,11 +10,24 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, WKCrownDelegate {
 
+    @IBOutlet weak var kilogramLabel: WKInterfaceLabel!
+    @IBOutlet weak var updateWeightButton: WKInterfaceButton!
+    @IBOutlet weak var weightSlider: WKInterfaceSlider!
+    
+    
+    var currentWeight: Double?
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
+        currentWeight = 75.5
+        weightSlider.setValue(Float(currentWeight!))
+        kilogramLabel.setText("\(currentWeight!)kg")
+        
+        crownSequencer.delegate = self
+        crownSequencer.focus()
         // Configure interface objects here.
     }
     
@@ -28,4 +41,47 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    func crownDidRotate(_ crownSequencer: WKCrownSequencer?, rotationalDelta: Double) {
+        
+        let newValue = currentWeight! + Double(rotationalDelta)
+        if newValue < 1 {
+            currentWeight = 1
+        } else if newValue > 100 {
+            currentWeight = 100
+        } else {
+            currentWeight = newValue
+        }
+        
+        let weightLabelText = String(round(currentWeight!*10)/10)
+        //currentWeight = currentWeight?.rounded()
+        kilogramLabel.setText("\(weightLabelText)kg")
+        weightSlider.setValue(Float(currentWeight!))
+    }
+    
+    @IBAction func updateWeightButtonPressed() {
+        
+        
+        
+    }
+    
+    @IBAction func weightSliderPressed(_ value: Float) {
+        
+        
+        
+        //let newValue = currentWeight! + Double(value)
+        print(Double(value))
+        if value < 1 {
+            currentWeight = 1
+        } else if value > 100 {
+            currentWeight = 100
+        } else {
+            currentWeight = Double(value)
+        }
+        let weightLabelText = String(round(currentWeight!*10)/10)
+        //currentWeight = currentWeight?.rounded()
+        kilogramLabel.setText("\(weightLabelText)kg")
+        weightSlider.setValue(Float(currentWeight!))
+    }
+    
+    
 }
